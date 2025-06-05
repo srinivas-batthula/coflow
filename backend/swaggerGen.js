84666b3ac937b35342b3a132b49d7a6f1c06cda0
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2025-06-05 19:00:25 +0530

    added code for swagger autoGen

---
+++ backend/swaggerGen.js
@@ -0,0 +1,21 @@
+// swaggerGen.js
+const swaggerAutogen = require("swagger-autogen")();
+
+
+const doc = {
+    info: {
+        title: "~HackPilot, A-Z hackathon manager",
+        description: "Automatically generated API documentation  ~HackPilot",
+        version: "1.0.0",
+    },
+    // host: "verseify-backend-kxtc.onrender.com", // Change this if deploying
+    schemes: ["https", "http"],
+    basePath: "/api",
+}
+
+const outputFile = "./swagger.json"           // The output file
+const routes = ["./routes/*.js"]         // Add your route files
+
+swaggerAutogen(outputFile, routes, doc).then(() => {
+    console.log("Swagger documentation generated successfully!")
+})
