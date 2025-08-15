7d9979d642c83ade7138344ea5a72e52a5a24002
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2025-08-15 18:17:01 +0530

    tests modifiead

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -1,6 +1,6 @@
 // tests/setup.js
 const mongoose = require('mongoose');
-// const { MongoMemoryServer } = require('mongodb-memory-server');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 const seedDatabase = require('./seed');
 require('dotenv').config({path:'./config.env'})
 require('jest-extended');
@@ -10,23 +10,23 @@ let mongoServer;
 
 beforeAll(async () => {
     try {
-        await mongoose.connect(process.env.Mongo_DB_URI);   // Directly Run Tests in Cloud-Atlas DB...
+        // await mongoose.connect(process.env.Mongo_DB_URI);   // Directly Run Tests in Cloud-Atlas DB...
 
 
-        // let memUri;
-        // console.log(process.env.BACKEND_ENV);
+        let memUri;
+        console.log(process.env.BACKEND_ENV);
 
-        // if (process.env.BACKEND_ENV === 'test') {      // Test environment (In CI/CD)...
-        //     mongoServer = await MongoMemoryServer.create();
-        //     memUri = mongoServer.getUri();
-        // }
-        // else {                                      // In Development...
-        //     memUri = 'mongodb://localhost:27017/testdb';
-        // }
+        if (process.env.BACKEND_ENV === 'test') {      // Test environment (In CI/CD)...
+            mongoServer = await MongoMemoryServer.create();
+            memUri = mongoServer.getUri();
+        }
+        else {                                      // In Development...
+            memUri = 'mongodb://localhost:27017/testdb';
+        }
 
-        // await mongoose.connect(memUri);
+        await mongoose.connect(memUri);
 
-        // await seedDatabase();       // Add Data to local-DB for testing...
+        await seedDatabase();       // Add Data to local-DB for testing...
     }
     catch (err) {
         console.error('Error in beforeAll-tests/ : -> ', err);
