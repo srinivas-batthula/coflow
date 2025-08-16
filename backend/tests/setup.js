c02569a5e10c25a33e1c1ab31d03c2f94bac3785
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2025-08-16 06:30:52 +0530

    tests2

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -2,10 +2,9 @@
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');         // Comment in Development Mode &&  Use only in CI/CD...
 const seedDatabase = require('./seed');
-require('dotenv').config({path:'./config.env'})
 require('jest-extended');
 
-let mongoServer; 
+let mongoServer;
 
 
 beforeAll(async () => {
@@ -13,9 +12,15 @@ beforeAll(async () => {
         // await mongoose.connect(process.env.Mongo_DB_URI);   // Directly Run Tests from Cloud-Atlas DB only in Local-DEV...
 
         console.log(process.env.BACKEND_ENV);
+        let memUri;
 
-        mongoServer = await MongoMemoryServer.create();
-        const memUri = mongoServer.getUri();
+        if (BACKEND_ENV === 'test') {
+            mongoServer = await MongoMemoryServer.create();
+            memUri = mongoServer.getUri();
+        }
+        else {
+            memUri = "mongodb://localhost:27017/testdb";
+        }
 
         await mongoose.connect(memUri);
 
